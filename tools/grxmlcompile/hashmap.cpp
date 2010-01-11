feac12bb0aed69d0a162581c67ba8b247b59e74f
Hieu Tran <htran@viosoft.com>
2010-01-11 15:11:38 -0800

    Include cstdio
    Signed-off-by: Chris Dearman <chris@mips.com>

--- tools/grxmlcompile/hashmap.cpp
+++ tools/grxmlcompile/hashmap.cpp
@@ -25,6 +25,7 @@
  *                                                                           *
  *---------------------------------------------------------------------------*/
 
+#include <cstdio>
 #include <map>
 #include <string>
 #include <iostream>
